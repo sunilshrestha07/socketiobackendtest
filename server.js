8f742551e045a5a7dd75ec0e9d3595d33f50e781
Your Name <sunilcrestha9@gmai.com>
2025-01-16 13:09:09 +0545

    sdjfsdh

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
